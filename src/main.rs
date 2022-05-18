8a9a2a5bd4773ae01c031196ef87e66f9d75b9a0
Francisco Krause Arnim <fkrausear@gmail.com>
2022-05-18 18:49:46 -0300

    Empty main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-use core::fmt;
-use programming_bitcoin::point::Point;
-use programming_bitcoin::finite_field::FieldElement;
-
-fn main() {
-    let test_field_0 = FieldElement::new(4, 4);
-    let test_field_1 = FieldElement::new(4, 4);
-    let test_field_2 = test_field_1 * test_field_0;
-    println!("{}", test_field_2);
-    // println!("{}", test_field == test_field);
-}
+fn main() {}
